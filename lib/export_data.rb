8f627269d841383a94787826b2b67c4ded73d647
RONAK BHATT <ronakabhattrz@gmail.com>
2023-09-05 19:09:18 +0530

    V3 export_to_text added

--- lib/export_data.rb
+++ lib/export_data.rb
@@ -12,17 +12,26 @@ module DataExport
     # Exports data to a CSV file.
     #
     # This method takes an array of objects and exports them to a CSV file
-    # specified by the `file_path`. It expects that the first object in the
-    # array defines the column names for the CSV file.
+    # specified by the `file_path`. It expects that each object's `attributes`
+    # method returns a hash of attribute names and values. The method first
+    # writes the header row using the attribute names from the first object,
+    # and then writes the data rows using the attribute values of each object.
     #
-    # @param objects [Array] The objects to export to CSV.
+    # @param objects [Array] The objects to export to a CSV file.
     # @param file_path [String] The path to the CSV file to be created.
     #
-    # @example Export user data to CSV:
+    # @example Export user data to a CSV file:
     #   objects = [user1, user2]
     #   DataExport::Exporter.export_to_csv(objects, 'users.csv')
     #
     # @return [void]
+    #
+    # @note If the `objects` array is empty, this method will output a message and
+    #   not create a file.
+    # @raise [CSV::MalformedCSVError] If an error occurs while writing to the CSV file,
+    #   an error message will be displayed.
+    # @raise [Errno::ENOENT] If an error occurs while creating or writing to the file,
+    #   an error message will be displayed.
     def self.export_to_csv(objects, file_path)
       begin
         CSV.open(file_path, 'w') do |csv|
@@ -46,6 +55,27 @@ module DataExport
       end
     end
     
+    # Exports data to a text file.
+    #
+    # This method takes an array of objects and exports them to a text file
+    # specified by the `file_path`. It expects that each object's `attributes`
+    # method returns an array of attribute values. Each attribute value is joined
+    # using a tab character ('\t') as a delimiter to create tab-separated values
+    # in the text file.
+    #
+    # @param objects [Array] The objects to export to a text file.
+    # @param file_path [String] The path to the text file to be created.
+    #
+    # @example Export user data to a text file:
+    #   objects = [user1, user2]
+    #   DataExport::Exporter.export_to_text(objects, 'users.txt')
+    #
+    # @return [void]
+    #
+    # @note If the `objects` array is empty, this method will output a message and
+    #   not create a file.
+    # @raise [Errno::ENOENT] If an error occurs while creating or writing to the file,
+    #   an error message will be displayed.
     def self.export_to_text(objects, file_path)
       begin
         File.open(file_path, 'w') do |file|
